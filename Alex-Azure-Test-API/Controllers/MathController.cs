5f7397c4861abbce3394cfe11548ccf741ea4996
Alex Gilmer <alex.gilmer@mitt.ca>
2023-04-12 15:35:18 -0500

    Add project files.

---
+++ Alex-Azure-Test-API/Controllers/MathController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Alex_Azure_Test_API.Controllers
+{
+    [Route("api/[controller]/[action]/{val1?}/{val2?}")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Add(double val1, double val2)
+        {
+            return Ok(val1 + val2);
+        }
+
+        [HttpGet]
+        public IActionResult Subtract(double val1, double val2)
+        {
+            return Ok(val1 - val2);
+        }
+
+        [HttpGet]
+        public IActionResult Multiply(double val1, double val2)
+        {
+            return Ok(val1 * val2);
+        }
+
+        [HttpGet]
+        public IActionResult Divide(double val1, double val2)
+        {
+            if (val2 == 0)
+            {
+                return BadRequest("Cannot divide by zero");
+            }
+
+            return Ok(val1 / val2);
+        }
+    }
+}
